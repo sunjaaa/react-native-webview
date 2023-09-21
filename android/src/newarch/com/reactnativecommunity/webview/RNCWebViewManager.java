b65d11fb938736f0a5ba9c69ab1c04163ff498b2
sunjaaa <senju5585@naver.com>
2023-09-21 12:07:20 +0900

    add: kakao intent

--- android/src/newarch/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/newarch/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -32,6 +32,9 @@ import org.json.JSONObject;
 
 import java.util.Map;
 
+import android.content.Intent;
+import java.net.URISyntaxException;
+
 @ReactModule(name = RNCWebViewManagerImpl.NAME)
 public class RNCWebViewManager extends SimpleViewManager<RNCWebView>
         implements RNCWebViewManagerInterface<RNCWebView> {
@@ -541,4 +544,46 @@ public class RNCWebViewManager extends SimpleViewManager<RNCWebView>
         mRNCWebViewManagerImpl.onDropViewInstance(view);
         super.onDropViewInstance(view);
     }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+    Log.d(TAG, request.getUrl().toString());
+    final String url = request.getUrl().toString();
+
+    ReactContext mReactContext = (ReactContext) view.getContext();
+
+    if (url == null || url.startsWith("http://") || url.startsWith("https://")) {
+        Log.d(TAG, "url is not intent");
+        return this.shouldOverrideUrlLoading(view, url);
+    }
+
+    try {
+        // Intent 생성
+        Intent intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+        PackageManager packageManager = mReactContext.getPackageManager();
+
+        //실행 가능한 앱이 있으면 앱 실행
+        if (intent.resolveActivity(packageManager) != null) {
+        Log.d(TAG, "has APP");
+        mReactContext.startActivity(intent);
+        Log.d(TAG, "ACTIVITY: ${intent.`package`}");
+        return true;
+        }
+
+        // Fallback URL이 있으면 현재 웹뷰에 로딩
+        String fallbackUrl = intent.getStringExtra("browser_fallback_url");
+        if (fallbackUrl != null) {
+        Log.d(TAG, "has fallbackUrl");
+        view.loadUrl(fallbackUrl);
+        Log.d(TAG, "FALLBACK: $fallbackUrl");
+        return true;
+        }
+        Log.e(TAG, "Could not parse anythings");
+    } catch (URISyntaxException e){
+        Log.e(TAG, "Invalid intent request", e);
+    }
+
+    return this.shouldOverrideUrlLoading(view, url);
+}
 }
